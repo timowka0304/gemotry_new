802362ddd59fb489eb4c6a97e4814bd332b0a169
timowka0304 <timoti20@mail.ru>
2019-05-20 12:10:01 +0700

    Change void to int

--- src/cross.c
+++ src/cross.c
@@ -3,11 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Cross(int x1, int y1, int r1, int x2, int y2, int r2)
+int Cross(int x1, int y1, int r1, int x2, int y2, int r2)
 {
     int dis = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
     if (dis <= (r1 + r2))
-        printf("1 and 2 circules are cross\n");
+        return 1;
     else
-        printf("1 and 2 circules are not cross\n");
+        return 0;
 }
